8fad388ed236eedb1b4ea769ae090113d523766f
RRR9 <43386258+RRR9@users.noreply.github.com>
2022-04-03 21:00:05 +0500

    Number of divisors

--- numberDivisors.hs
+++ numberDivisors.hs
@@ -5,6 +5,6 @@ dividers' :: Integer -> [Integer] -> Integer -> [Integer]
 dividers' n l d =
     if d * 2 <= n then
         if n `mod` d == 0 then
-		    dividers' n (d:l) (d + 1)
+            dividers' n (d:l) (d + 1)
         else dividers' n l (d + 1)
     else l
