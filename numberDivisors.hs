6b94129a37d21f6e06aeb08a02d38ba6cd27cebe
RRR9 <43386258+RRR9@users.noreply.github.com>
2022-04-03 20:58:55 +0500

    Update numberDivisors.hs

--- numberDivisors.hs
+++ numberDivisors.hs
@@ -3,8 +3,8 @@ dividers n = dividers' n [1] 2
 
 dividers' :: Integer -> [Integer] -> Integer -> [Integer]
 dividers' n l d =
-    if d * 2 <= n
-    then    if n `mod` d == 0
-            then dividers' n (d:l) (d + 1)
-            else dividers' n l (d + 1)
+    if d * 2 <= n then
+        if n `mod` d == 0 then
+		    dividers' n (d:l) (d + 1)
+        else dividers' n l (d + 1)
     else l
